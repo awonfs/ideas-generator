0ab7241860f51555f6f04fd1ab670df9d42e35e6
Eetu <eetu.kristian.ronkko@gmail.com>
2023-06-10 17:36:53 +0300

    added prisma and made a Prompt model

---
+++ prisma/migrations/20230610143619_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Prompt" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "text" TEXT NOT NULL,
+    "userId" TEXT NOT NULL
+);
