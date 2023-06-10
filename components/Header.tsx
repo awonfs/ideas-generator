84cd9ecdb24853e8a57a152f96a9c69709a6c500
Eetu <eetu.kristian.ronkko@gmail.com>
2023-06-10 15:01:12 +0300

    Rendering links in nav based if logged in or not

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,6 +28,15 @@ function Header() {
             {isSignedIn ? (
               <>
                 <li>
+                  <Link
+                    href="/generate-ideas"
+                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                    aria-current="page"
+                  >
+                    Generate ideas
+                  </Link>
+                </li>
+                <li>
                   <UserButton afterSignOutUrl="/" />
                 </li>
               </>
