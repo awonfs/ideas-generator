a875e88eec72d65a326391685bc06c64c1f39c20
Eetu <eetu.kristian.ronkko@gmail.com>
2023-06-11 13:48:24 +0300

    Added a spinner to ideas form so UX is better

--- components/IdeasForm.tsx
+++ components/IdeasForm.tsx
@@ -3,6 +3,8 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import Spinner from "@/components/Spinner";
+import { useState } from "react";
 
 const formSchema = z.object({
   prompt: z.string().min(1, "This field cant be empty").max(1000),
@@ -16,6 +18,8 @@ type FormProps = {
 };
 
 function IdeasForm({ label, placeholder }: FormProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -26,16 +30,19 @@ function IdeasForm({ label, placeholder }: FormProps) {
   });
 
   async function onSubmit(data: Input) {
+    setIsLoading(true); // start loading
     try {
       const response = await axios.post(
         "http://localhost:3000/api/generate",
         data
       );
       console.log(response.data);
-      reset();
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
+    reset();
   }
 
   return (
@@ -49,13 +56,19 @@ function IdeasForm({ label, placeholder }: FormProps) {
       >
         {label}
       </label>
-      <textarea
-        id="prompt"
-        className="block w-full text-sm rounded-lg border  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-        placeholder={placeholder}
-        rows={6}
-        {...register("prompt")}
-      ></textarea>
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <Spinner />
+        </div>
+      ) : (
+        <textarea
+          id="prompt"
+          className="block w-full text-sm rounded-lg border  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+          placeholder={placeholder}
+          rows={6}
+          {...register("prompt")}
+        ></textarea>
+      )}
       <button
         type="submit"
         className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
